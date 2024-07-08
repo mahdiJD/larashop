3161818585923cb39186e73f8c4651deaed38a9a
Mahdi <bijarimahdi@gmail.com>
2024-07-08 09:35:27 +0330

    complit migration categories and prop_cat

--- database/migrations/2024_07_07_134339_product_categories.php
+++ database/migrations/2024_07_07_134339_product_categories.php
@@ -11,7 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('product_categorie',function(Blueprint $table){
+            $table->foreignId('product_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('categorie_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('image_tag');
     }
 };
