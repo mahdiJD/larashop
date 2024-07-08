c9b0a07d54aee323ce4cf65acb3664194af79d12
Mahdi <bijarimahdi@gmail.com>
2024-07-08 09:39:04 +0330

    complit product factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -16,8 +16,23 @@ class ProductFactory extends Factory
      */
     public function definition(): array
     {
+        $categories = [
+            'Fruits',
+            'Vegetables',
+            'Bread',
+            'Meat',
+        ];
         return [
-            //
+            'name' => $title = substr(fake()->sentence(),20),
+            'slug' => str()->slug($title),
+            'file' => fake()->imageUrl($width= 1920 ,$height= 1280),
+            'weight' => rand(150,250),
+            'description' => fake()->realText(),
+            'bio' => fake()->sentence(),
+            'price' => rand(3,16),
+            'categories' => $categories[rand(0,3)],
+            'is_published' => true ,
+            'user_id' => rand(5,6),
         ];
     }
 }
